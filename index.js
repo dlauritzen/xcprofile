f0cfd08196ec9bfc2b11ed3ab4075fd69bbe4803
Dallin Lauritzen <dallin.lauritzen@parentlink.net>
2014-01-13 11:41:17 -0700

    Replace search method with KMP algorithm

--- index.js
+++ index.js
@@ -5,6 +5,61 @@ var fs = require('fs');
 var path = require('path');
 var plistlib = require('plistlib');
 
+function buildKmpT(w) {
+	var t = new Array();
+	t.length = w.length;
+
+	var pos = 2;
+	var cnd = 0;
+	t[0] = -1;
+	t[1] = 0;
+
+	while (pos < w.length) {
+		if (w[pos - 1] == w[cnd]) {
+			// Substring continues
+			cnd++;
+			t[pos] = cnd;
+			pos++;
+		}
+		else if (cnd > 0) {
+			// Can fall back
+			cnd = t[cnd];
+		}
+		else {
+			// No candidates. cnd = 0
+			t[pos] = 0;
+			pos++;
+		}
+	}
+
+	return t;
+}
+
+function kmp(s, w, t) {
+	var m = 0;
+	var i = 0;
+	var t = t || buildKmpT(w);
+
+	while ((m + i) < s.length) {
+		if (w[i] == s[m + i]) {
+			if (i == w.length - 1) {
+				return m;
+			}
+			i++;
+		}
+		else {
+			m = m + i - t[i];
+			if (t[i] > -1) {
+				i = t[i];
+			}
+			else {
+				i = 0;
+			}
+		}
+	}
+	return -1;
+}
+
 function indexOfBuffer(haystack, needle) {
 	var matchedIndex = 0;
 	for (var i = 0; i < haystack.length; i++) {
@@ -31,10 +86,13 @@ exports.load = function(filename, done) {
 		if (err) return done(err);
 		// console.log('File loaded. Length: %d bytes.', data.length);
 
-		var startIndex = indexOfBuffer(data, startBuffer);
+		// var startIndex = indexOfBuffer(data, startBuffer);
+		var startIndex = kmp(data, startBuffer);
 		if (startIndex == -1) return done(new Error('Plist not found in provisioning profile.'));
 		// console.log('Plist found at index %d', startIndex);
-		var endIndex = indexOfBuffer(data, endBuffer);
+
+		// var endIndex = indexOfBuffer(data, endBuffer);
+		var endIndex = kmp(data, endBuffer);
 		if (endIndex == -1) return done(new Error('Programmer\'s error. Could not find end of plist.'));
 		endIndex += endBuffer.length; // this originally points at the beginning of the end
 		// console.log('Plist ends at index %s', endIndex);
